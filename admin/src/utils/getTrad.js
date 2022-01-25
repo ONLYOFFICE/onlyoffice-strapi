8175ed67971025fb028d851d2eadf00ca4fdfb20
Oleg Sinizin <oleg.sinizin@onlyoffice.com>
2022-01-25 19:25:31 +0300

    update translations

---
+++ admin/src/utils/getTrad.js
@@ -0,0 +1,9 @@
+/**
+ * Copyright (c) Ascensio System SIA 2022. All rights reserved.
+ * http://www.onlyoffice.com
+ **/
+import pluginId from '../pluginId';
+
+const getTrad = id => `${pluginId}.${id}`;
+
+export default getTrad;
