a3ddeba0ad72fabe8963e24a60a9f1232c0e9294
Oleg Sinizin <oleg.sinizin@onlyoffice.com>
2022-01-30 18:32:20 +0300

    update plugin home page

--- admin/src/pages/HomePage/index.js
+++ admin/src/pages/HomePage/index.js
@@ -14,38 +14,46 @@ import {useIntl} from 'react-intl';
 import {
   LoadingIndicatorPage,
   SearchURLQuery,
-  useQueryParams,
-  DynamicTable,
-  NoContent,
   useRBAC
 } from '@strapi/helper-plugin';
 import Editor from '../Editor';
-import {ContentLayout, ActionLayout} from '@strapi/design-system/Layout';
+import {ContentLayout} from '@strapi/design-system/Layout';
 import PaginationFooter from "../../components/PaginationFooter";
 import OnlyofficeLogo from "../../components/OnlyofficeLogo";
 import CenterActionLayout from '../../components/CenterActionLayout';
-import {isFileEditable, isFileViewable} from '../../utils/fileUtility';
 import pluginId from "../../pluginId";
 import axiosInstance from "../../utils/axiosInstance";
-import matchSorter from 'match-sorter';
 import tableHeaders from "./utils/tableHeaders";
 import permissions from "../../permissions";
 import TableRows from "./TableRows";
+import {useQuery} from 'react-query';
+import {fetchFiles} from '../utils/api';
+import {Main} from '@strapi/design-system/Main';
+import getTrad from '../../utils/getTrad';
+import DynamicTable from './DynamicTable';
+import {EmptyStateLayout} from '@strapi/design-system/EmptyStateLayout';
+import EmptyDocuments from '@strapi/icons/EmptyDocuments';
 
+const uploadPluginPermissions = {
+  read: [{action: 'plugin::upload.read', subject: null}],
+  update: [{action: 'plugin::upload.assets.update', subject: null, fields: null}],
+};
 
 const HomePage = () => {
   const {formatMessage} = useIntl();
-  const [{query}] = useQueryParams();
-  const _q = query?._q || '';
-  const {pathname} = useLocation();
+  const {pathname, search} = useLocation();
   const {push} = useHistory();
   const {allowedActions: {can0}} = useRBAC(permissions.settings); // check user has access to settings
+  const {allowedActions: {canRead, canUpdate}} = useRBAC(uploadPluginPermissions);
+  const queryName = ['files', search];
+  const {
+    data,
+    isFetching
+  } = useQuery(queryName, () => fetchFiles(search));
+  const filesCount = data?.pagination?.total || 0;
 
-  const [isLoading, setIsLoading] = useState(true);
   const [editorFile, setEditorFile] = useState(null);
-  const [files, setFiles] = useState([]);
   const [isEditor, setIsEditor] = useState(false);
-  const [filesPagination, setFilesPagination] = useState(null);
   const [docServConfig, setDocServConfig] = useState(null);
 
   const getDocServConfig = async () => {
@@ -64,95 +72,84 @@ const HomePage = () => {
       });
   }, []);
 
-  const updateFiles = () => {
-    axiosInstance.get('/upload/files')
-      .then((result) => {
-        const tmp = [];
-        result.data.results.forEach((file) => {
-          if (isFileViewable(file.ext) || isFileEditable(file.ext)) {
-            tmp.push(file);
-          }
-        });
-
-        setFiles(tmp);
-        setIsLoading(false);
-        setFilesPagination(result.data.pagination);
-      });
-  }
-
-  useEffect(() => {
-    let refreshInterval;
-    setFiles([]);
-    if (!isEditor) {
-      updateFiles();
-      setIsLoading(true);
-      refreshInterval = setInterval(() => {
-        updateFiles();
-      }, 5000);
+  const openEditor = (file) => {
+    if (canRead || canUpdate) {
+      setEditorFile(file);
+      setIsEditor(true);
     }
+  };
 
-    return () => clearInterval(refreshInterval);
-  }, [isEditor]);
-
-  const sortedFiles = matchSorter(files || [], _q, {keys: ['name', 'type', 'size', 'last modified']});
-  const emptyContent = _q && !sortedFiles.length ? 'search' : 'files';
-
-  if (isLoading || filesPagination === null) {
+  if (isFetching) {
     return <LoadingIndicatorPage/>;
   }
 
-  if (!isLoading && docServConfig && docServConfig.docServUrl === null && !can0) {
-    return <NoContent content={{
-      id: 'onlyoffice.docserv-url.empty',
-      defaultMessage: 'Document Server Address is not set'
-    }}/>
+  if (docServConfig && docServConfig.docServUrl === null && !can0 && !isFetching) {
+  return (
+    <Main>
+      <CenterActionLayout
+        startActions={
+          <OnlyofficeLogo/>
+        }
+      />
+      <ContentLayout>
+        <EmptyStateLayout
+          icon={<EmptyDocuments width="10rem"/>}
+          content={formatMessage(
+            {id: getTrad('onlyoffice.docserv-url.empty'), defaultMessage: 'Document Server Address is not set'}
+          )}
+        />
+      </ContentLayout>
+    </Main>
+  );
   } else if (can0 && docServConfig && docServConfig.docServUrl === null) {
     push({
       pathname: `${pathname.replace(`/plugins/${pluginId}`, `/settings/${pluginId}`)}`
     });
   }
 
+  if (isEditor && editorFile) {
+    return <Editor file={editorFile} docServConfig={docServConfig} canEdit={canUpdate} canRead={canRead}/>
+  }
+
   return (
-    <>
-      {isEditor && editorFile ? (
-        <Editor file={editorFile} docServConfig={docServConfig}/>
-      ) : (
-        <>
-          <CenterActionLayout
-            startActions={
-              <OnlyofficeLogo/>
-            }
+    <Main>
+      <CenterActionLayout
+        startActions={
+          <OnlyofficeLogo/>
+        }
+      />
+      <CenterActionLayout
+        startActions={
+          formatMessage({
+              id: getTrad('onlyoffice.content.files-multiple'),
+              defaultMessage: '0 files',
+            },
+            {number: filesCount}
+          )}
+        endActions={
+          <SearchURLQuery
+            label={formatMessage({
+              id: getTrad('onlyoffice.files.list.files-search'),
+              defaultMessage: 'Search',
+            })}
           />
-          {files?.length ? (
-            <ActionLayout
-              endActions={
-                <SearchURLQuery
-                  label={formatMessage({
-                    id: 'onlyoffice.files.list.files-search',
-                    defaultMessage: 'Search',
-                  })}
-                />
-              }
-            />
-          ) : <></>}
-          <ContentLayout>
-            {/*{sortedFiles && sortedFiles?.length ?*/}
-            {/*  (<>*/}
-            <DynamicTable
-              isLoading={isLoading}
-              headers={tableHeaders}
-              rows={sortedFiles}
-            >
-              {/*<TableRows*/}
-              {/*  headers={tableHeaders}*/}
-              {/*  rows={sortedFiles}*/}
-              {/*/>*/}
-            </DynamicTable>
-            <PaginationFooter pagination={filesPagination}/>
-          </ContentLayout>
-        </>
-      )}
-    </>
+        }
+      />
+      <ContentLayout>
+        <DynamicTable
+          isLoading={isFetching}
+          headers={tableHeaders}
+          rows={data.results}
+        >
+          <TableRows
+            headers={tableHeaders}
+            rows={data.results}
+            openEditor={openEditor}
+          />
+        </DynamicTable>
+        <PaginationFooter pagination={data.pagination}/>
+      </ContentLayout>
+    </Main>
   );
 };
 
