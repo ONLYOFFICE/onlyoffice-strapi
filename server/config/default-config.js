a1a93c326e6c428be0c910f2eb3ef4e39a9c5974
Sergey Linnik <sergey.linnik@onlyoffice.com>
2022-04-06 13:31:09 +0300

    1.0.1

--- server/config/default-config.js
+++ server/config/default-config.js
@@ -18,7 +18,7 @@
 module.exports = {
   openapi: "3.0.0",
   info: {
-    version: '1.0.0',
+    version: '1.0.1',
     title: 'ONLYOFFICE',
     description: 'The integration app allows users to view, edit, and co-author office documents, spreadsheets, and presentations added to the Strapi Media Library using ONLYOFFICE Docs.',
     contact: {
